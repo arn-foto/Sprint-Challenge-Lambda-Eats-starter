66a224c24d8ca87db720dc70472dc74acc5a2f5a
Anthony <38259824+arn-foto@users.noreply.github.com>
2020-05-22 11:11:32 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React from 'react';
+import Pizza  from './Pizza';
+import Home from './Home';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
-};
+function App() {
+	return (
+		<Router>
+			<nav className="navbar">
+				<Link to="/">
+					<button name="homebutton">Home</button>
+				</Link>
+				<br />
+				<Link to="/Pizza">
+					<button name="orderbutton">Order</button>
+				</Link>
+			</nav>
+			<div className="App">
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/Pizza" component={Pizza} />
+				</Switch>
+			</div>
+		</Router>
+	);
+}
+
 export default App;
